3fe108e177b3f503edf11e35c540a4d89d18cc69
Vaishali2803 <r.vaishali2803@gmail.com>
2025-10-31 11:14:45 +0530

    STOP WORDS.py

--- STOP WORDS.py
+++ STOP WORDS.py
@@ -1,10 +1,13 @@
 import spacy
 nlp=spacy.load('en_core_web_sm')
-d=nlp(u"I am a runner running in a race because I love to run since I ran today")
-for t in d:
-    print(t.text,t.pos_,t.lemma,t.lemma_)
-
-def show_lemma(text):
-    for t in  text:
-        print(t.text,t.pos_,t.lemma,t.lemma_)
-show_lemma(d)
+print(nlp.Defaults.stop_words)
+print(len(nlp.Defaults.stop_words))
+print(nlp.vocab['mystery'].is_stop)
+print(nlp.vocab['even'].is_stop)
+nlp.Defaults.stop_words.add('btw') #add stop word
+nlp.vocab['btw'].is_stop=True #optional
+print(nlp.vocab['btw'].is_stop)
+print(len(nlp.Defaults.stop_words))
+nlp.Defaults.stop_words.remove("show") # remove stop word
+print(nlp.vocab['four'].is_stop)
+print(len(nlp.Defaults.stop_words))
